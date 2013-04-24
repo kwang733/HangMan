04ab0da1b08695c646bde9dbde54dacc31f7b460
kwang733 <kevinwang@fas.harvard.edu>
2013-04-23 22:15:33 -0300

    Update ViewController.h

--- Hangman/Hangman/ViewController.h
+++ Hangman/Hangman/ViewController.h
@@ -1,11 +1,3 @@
-//
-//  ViewController.h
-//  Hangman
-//
-//  Created by D.J on 4/17/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController <UITextFieldDelegate>
