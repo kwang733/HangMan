5756db03723ff6ec11e861f639eaa6e32d007857
kwang733 <kevinwang@fas.harvard.edu>
2013-04-23 22:15:52 -0300

    Update main.m

--- Hangman/Hangman/main.m
+++ Hangman/Hangman/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  Hangman
-//
-//  Created by D.J on 4/17/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
